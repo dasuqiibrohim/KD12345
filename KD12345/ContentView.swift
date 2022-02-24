a52112d99f48946dc76995f5f499556bb75321d0
Ibrohim Dasuqi <dasuqiibrohim@gmail.com>
2022-02-25 05:52:30 +0700

    First commit.

---
+++ KD12345/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KD12345
+//
+//  Created by Ibrohim Dasuqi on 25/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KD12345/KD12345App.swift
@@ -0,0 +1,17 @@
+//
+//  KD12345App.swift
+//  KD12345
+//
+//  Created by Ibrohim Dasuqi on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct KD12345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
